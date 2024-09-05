751d45f28b2e315844a209074556f10965a91031
Eugene Molchanov <eugene.a.molchanov@gmail.com>
2024-09-05 20:32:09 +0200

    code.404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,34 @@
-'use client';
+import * as React from 'react';
+
+import type { Metadata } from 'next';
+
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+
+import { ApplicationToolbar } from '@/modules/application-toolbar';
+import { getMetadata, RenderMetadata } from '@/modules/metadata';
+
+import { PageContent } from '@/shared/components/page-content';
+import { ProductCard } from '@/shared/components/product-card';
+import { getAppQueryClient } from '@/shared/modules/swr';
+
+type PageProps = {
+  searchParams: Next.PageSearchParams;
+};
+
+export const dynamic = 'force-dynamic';
+
+export async function generateMetadata({ searchParams }: PageProps): Promise<Metadata> {
+  return getMetadata({ variant: 'summary' });
+}
 
 export default function NotFound() {
-  return <>Could not find requested resource</>;
+  const queryClient = getAppQueryClient();
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <HydrationBoundary state={dehydratedState}>
+      <ApplicationToolbar />
+      <PageContent>Not found</PageContent>
+    </HydrationBoundary>
+  );
 }

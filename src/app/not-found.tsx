d9d0c8c947ab4de7d4c6ef62099637daade13b30
Eugene Molchanov <eugene.a.molchanov@gmail.com>
2024-09-06 20:47:28 +0200

    timestamp.hash.4

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from 'next';
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
 import { ApplicationToolbar } from '@/modules/application-toolbar';
-import { getMetadata } from '@/modules/metadata';
+import { getMetadata, getMetadataHash } from '@/modules/metadata';
 
 import { PageContent } from '@/shared/components/page-content';
 import { getAppQueryClient } from '@/shared/modules/swr';
@@ -23,16 +23,18 @@ export async function generateMetadata({ searchParams }: PageProps): Promise<Met
     title: '404',
     description: 'Not found',
     image: 'https://images.unsplash.com/photo-1618044619888-009e412ff12a?q=80&w=2371&auto=format&fit=cover',
+    hash: getMetadataHash(),
   });
 }
 
 export default function NotFound() {
   const queryClient = getAppQueryClient();
   const dehydratedState = dehydrate(queryClient);
+  const hash = getMetadataHash();
 
   return (
     <HydrationBoundary state={dehydratedState}>
-      <ApplicationToolbar />
+      <ApplicationToolbar hash={hash} />
       <PageContent>Not found</PageContent>
     </HydrationBoundary>
   );

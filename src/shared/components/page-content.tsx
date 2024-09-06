422a73d2af62c9de94b3c610fb6a83494cfe02ad
Eugene Molchanov <eugene.a.molchanov@gmail.com>
2024-09-06 10:10:53 +0200

    ui.updates

--- src/shared/components/page-content.tsx
+++ src/shared/components/page-content.tsx
@@ -6,16 +6,18 @@ import * as MUI from '@mui/material';
 
 export function PageContent(props: PropsWithChildren) {
   return (
-    <MUI.Stack
-      direction="column"
-      justifyContent="center"
-      alignContent="center"
-      alignItems="center"
-      flexWrap="wrap"
-      gap={10}
-      marginBlock={10}
-    >
-      {props.children}
-    </MUI.Stack>
+    <MUI.Container>
+      <MUI.Stack
+        direction="column"
+        justifyContent="center"
+        alignContent="center"
+        alignItems="center"
+        flexWrap="wrap"
+        gap={10}
+        marginBlock={10}
+      >
+        {props.children}
+      </MUI.Stack>
+    </MUI.Container>
   );
 }
